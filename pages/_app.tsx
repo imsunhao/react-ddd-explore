5cc9d19c73454231372c09786184288ca2aa58d7
sunhao <imsunhao@vip.163.com>
2022-01-29 11:51:41 +0800

    feat: 复现 错误
      - Cannot update a component (`Provider`) while rendering a different component (`Provider`)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app'
+import AppProvider from 'app/App'
 import DatasProvider from 'datas/app/domain'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DatasProvider>
-      <Component {...pageProps} />
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
     </DatasProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   const { dataPool } = useDatas()
+  console.log('页面重新刷新')
   return <div>Data Pool: {JSON.stringify(dataPool, null, 2)}</div>
 }
 
